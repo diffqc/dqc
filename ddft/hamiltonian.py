972709c9e68b14a9965c95add743ffa5d0c93881
mfkasim91 <firman.kasim@gmail.com>
2020-01-09 14:33:00 +0000

    Add return_hamiltonian option in HamiltonNP1D

--- ddft/hamiltonian.py
+++ ddft/hamiltonian.py
@@ -70,6 +70,8 @@ class HamiltonNP1D(torch.autograd.Function):
     * return_wv: bool
         If true, then return the wavefunction. Otherwise, it returns the total
         density. Default: True
+    * return_hamiltonian: bool
+        If true, then return the Hamiltonian. Default: False
 
     Returns
     -------
@@ -77,10 +79,13 @@ class HamiltonNP1D(torch.autograd.Function):
         Wavefunction or total density.
     * e: torch.tensor (nbatch, np)
         The energy of the wavefunction or density
+    * H: torch.tensor (nb, nr, nr) (only if return_hamiltonian==True)
+        The constructed Hamiltonian of the system.
     """
 
     @staticmethod
-    def forward(ctx, rgrid, vext, iexc=None, return_wf=True):
+    def forward(ctx, rgrid, vext, iexc=None, return_wf=True,
+            return_hamiltonian=False):
         nb = rgrid.shape[0]
         nr = rgrid.shape[-1]
         dr = rgrid[:,1] - rgrid[:,0] # (nb, )
@@ -105,59 +110,86 @@ class HamiltonNP1D(torch.autograd.Function):
         ctx.wf = wf
         ctx.H = H
         ctx.return_wf = return_wf
+        ctx.return_hamiltonian = return_hamiltonian
 
         if return_wf:
-            return wf, e
+            wf_or_n = wf
         else:
             # calculate the density
             n = (wf * wf).sum(dim=-1)
-            return n, e
+            wf_or_n = n
+
+        if return_hamiltonian:
+            return wf_or_n, e, H
+        else:
+            return wf_or_n, e
 
     @staticmethod
-    def backward(ctx, grad_wf_or_n, grad_e):
+    def backward(ctx, *grads):
+        # unpack the gradients
+        if ctx.return_hamiltonian:
+            grad_wf_or_n, grad_e, grad_H = grads
+        else:
+            grad_wf_or_n, grad_e = grads
+            grad_H = None
+
         nb = ctx.e.shape[0]
         np = ctx.e.shape[1]
         nr = ctx.H.shape[1]
         wf = ctx.wf
 
-        # iterate over the number of particles
-        for b in range(np):
-            # get the wavefunction and energy of this particle
-            wf = ctx.wf[:,:,b]
-            e = ctx.e[:,b]
-
-            # construct the matrix kernel
-            eI = e.repeat_interleave(nr).view(nb,nr).diag_embed()
-            A = eI - ctx.H
-            Ainv = torch.pinverse(A, rcond=1e-5) # (nb, nr, nr)
-
-            # calculate the grad_wf for this particle
-            if not ctx.return_wf:
-                grad_wfb = grad_wf_or_n * 2 * ctx.wf[:,:,b]
-            else:
-                grad_wfb = grad_wf_or_n[:,:,b] # (nb, nr)
-
-            # apply the inverse operation
-            # n.b.: the inverse output must be orthogonal to wf
-            gradb = grad_wfb.unsqueeze(-1)
-            Awf = torch.bmm(Ainv, gradb) # (nb, nr, 1)
-            Awf = Awf.squeeze(-1) # (nb, nr)
-            # orthogonalize w.r.t. wf
-            # wf already has norm == 1
-            proj_Awf = (Awf * wf).sum(dim=-1, keepdim=True) * wf
-            Awf_ortho = Awf - proj_Awf
-            grad_vext_wf = Awf_ortho * wf
-
-            # add the contribution from grad_e
-            grad_vext_e = wf * wf * grad_e[:,b]
-
-            # get the gradient of external potential
-            grad_vext_b = grad_vext_wf + grad_vext_e
-
-            # accummulate the gradient
-            if b == 0:
-                grad_vext = grad_vext_b
-            else:
-                grad_vext = grad_vext + grad_vext_b
-
+        grad_vext = _apply_G(ctx.wf, ctx.e, ctx.H, grad_wf_or_n,
+                              grad_e = grad_e,
+                              grad_H = grad_H,
+                              is_grad_wf = ctx.return_wf)
         return (None, grad_vext, None, None)
+
+def _apply_G(wfs, es, H, grad_wf_or_n, grad_e=None, grad_H=None, is_grad_wf=True):
+    nb, np = es.shape[:2]
+    nr = H.shape[1]
+
+    # iterate over the number of particles
+    for b in range(np):
+        # get the wavefunction and energy of this particle
+        wf = wfs[:,:,b]
+        e = es[:,b]
+
+        # construct the matrix kernel
+        eI = e.repeat_interleave(nr).view(nb,nr).diag_embed()
+        A = eI - H
+        Ainv = torch.pinverse(A, rcond=1e-5) # (nb, nr, nr)
+
+        # calculate the grad_wf for this particle
+        if not is_grad_wf:
+            grad_wfb = grad_wf_or_n * 2 * wf
+        else:
+            grad_wfb = grad_wf_or_n[:,:,b] # (nb, nr)
+
+        # apply the inverse operation
+        # n.b.: the inverse output must be orthogonal to wf
+        gradb = grad_wfb.unsqueeze(-1)
+        Awf = torch.bmm(Ainv, gradb) # (nb, nr, 1)
+        Awf = Awf.squeeze(-1) # (nb, nr)
+        # orthogonalize w.r.t. wf
+        # wf already has norm == 1
+        proj_Awf = (Awf * wf).sum(dim=-1, keepdim=True) * wf
+        Awf_ortho = Awf - proj_Awf
+        grad_vext_b = Awf_ortho * wf
+
+        # add the contribution from grad_e
+        if grad_e is not None:
+            grad_vext_e = wf * wf * grad_e[:,b]
+            grad_vext_b = grad_vext_b + grad_vext_e
+
+        # accummulate the gradient
+        if b == 0:
+            grad_vext = grad_vext_b
+        else:
+            grad_vext = grad_vext + grad_vext_b
+
+    # add the contribution from grad_H
+    if grad_H is not None:
+        grad_vext_H = torch.diagonal(grad_H, dim1=-2, dim2=-1)
+        grad_vext = grad_vext + grad_vext_H
+
+    return grad_vext
