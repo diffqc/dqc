0acdf80c4bc77d688f1b3beac9888ca72d5963de
mfkasim91 <firman.kasim@gmail.com>
2020-01-10 11:21:44 +0000

    Inverse transformation

--- ddft/transforms/base_transform.py
+++ ddft/transforms/base_transform.py
@@ -8,17 +8,37 @@ class BaseTransform(object):
     """
     @abstractmethod
     def __call__(self, x):
+        """
+        Applying the transformation to `x` with shape (nbatch, ninputs)
+        The output is (nbatch, noutputs).
+        """
         pass
 
     @abstractmethod
     def _transpose(self, y):
+        """
+        Applying the transpose transformation to `y` with shape
+        (nbatch, noutputs).
+        The output is (nbatch, ninputs).
+        """
         pass
 
     @property
     def T(self):
+        """
+        Return the transpose transformation object.
+        """
         if not hasattr(self, "_transpose_transform_"):
             self._transpose_transform_ = TransposeTransform(self)
         return self._transpose_transform_
+
+    def inv(self, **options):
+        """
+        Return the inverse transformation object.
+        """
+        if not hasattr(self, "_inverse_"):
+            self._inverse_ = InverseTransform(self, **options)
+        return self._inverse_
 
     def __add__(self, other):
         return AddTransform(self, other)
@@ -54,6 +74,29 @@ class TransposeTransform(BaseTransform):
     @property
     def T(self):
         return self.a
+
+class InverseTransform(BaseTransform):
+    def __init__(self, a, **options):
+        self.a = a
+        self.options = options
+
+    def __call__(self, x):
+        def loss(y):
+            return self.a(y) - x
+        jinv0 = 1.0
+        y0 = torch.zeros_like(x).to(x.device)
+        y = lbfgs(loss, y0, jinv0, **self.options)
+        return y
+
+    def _transpose(self, y):
+        def loss(x):
+            return self.a._transpose(x) - y
+        jinv0 = 1.0
+        x0 = torch.zeros_like(y).to(y.device)
+        x = lbfgs(loss, x0, jinv0, **self.options)
+        return x
+
+####################### arithmetics operation #######################
 
 class AddTransform(BaseTransform):
     def __init__(self, a, b):
