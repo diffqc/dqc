77f6c9b1869f40cb9162726d2fd0cdb3766bdb22
mfkasim91 <firman.kasim@gmail.com>
2020-01-15 14:40:40 +0000

    Cleaning up unnecessary files

--- ddft/utils/orthogonalize.py
+++
@@ -1,7 +0,0 @@
-def orthogonalize(a, b, dim=-1):
-    """
-    Orthogonalize a with respect to b.
-    The orthogonalization is done such that (a*b).sum(dim=dim) is equal to 0
-    """
-    proj = (a * b).sum(dim=dim, keepdim=True) / (b * b).sum(dim=dim, keepdim=True)
-    return a - proj * b
