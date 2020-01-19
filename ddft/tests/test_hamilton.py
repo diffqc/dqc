07c07b64f065243696e31e1b2fbc8c18cda3ed99
mfkasim91 <firman.kasim@gmail.com>
2020-01-19 22:08:33 +0000

    Test kinetics for 1-3 dimensions

--- ddft/tests/test_hamilton.py
+++ ddft/tests/test_hamilton.py
@@ -4,21 +4,27 @@ import matplotlib.pyplot as plt
 from ddft.spaces.qspace import QSpace
 from ddft.hamiltons.hamiltonpw import HamiltonPlaneWave
 
-def test_hamilton_pw_kinetics():
+def compare_hamilton_pw_kinetics(nx, ndim, boxsize=4.0):
     dtype = torch.float64
-    nr = 100
-    ndim = 1
-    rgrid = torch.linspace(-2, 2, nr+1)[:-1].unsqueeze(-1).to(dtype) # (nr,ndim)
+    x = torch.linspace(-boxsize/2.0, boxsize/2.0, nx+1)[:-1].to(dtype)
+    rgrids = torch.meshgrid(*[x for i in range(ndim)]) # (nx,nx)
+    rgrid = torch.cat([rgridx.unsqueeze(-1) for rgridx in rgrids], dim=-1).view(-1,ndim) # (nr,ndim)
+
+    # construct the wavefunction as wf = sumj(sin(2*pi*xj))
     wf = torch.sin(rgrid * 2 * np.pi).unsqueeze(0) # (1,nr,ndim)
+    wf = torch.sum(wf, dim=-1, keepdim=True) # (1,nr,1)
 
-    space = QSpace(rgrid, (rgrid.shape[0],))
+    space = QSpace(rgrid, [nx for i in range(ndim)])
     h = HamiltonPlaneWave(space)
-    kin = h.kinetics(wf) # (1,nr,ndim)
+    kin = h.kinetics(wf) # (1,nr,1)
 
-    dr = rgrid.squeeze()[1] - rgrid.squeeze()[0]
-    dq = space.qgrid.squeeze()[1] - space.qgrid.squeeze()[0]
-    assert torch.allclose(dr*dq / (2*np.pi/nr), torch.ones_like(dr), rtol=1e-4)
-    print(rgrid.squeeze())
-    print(space.qgrid.squeeze())
+    assert torch.allclose(wf-2*kin/(2*np.pi)**2, torch.zeros_like(wf), atol=1e-4)
 
-    assert torch.allclose(wf+kin/(2*np.pi)**2, torch.zeros_like(wf), atol=1e-4)
+def test_hamilton_pw_kinetics():
+    compare_hamilton_pw_kinetics(100, 1)
+
+def test_hamilton_pw_kinetics2d():
+    compare_hamilton_pw_kinetics(50, 2)
+
+def test_hamilton_pw_kinetics3d():
+    compare_hamilton_pw_kinetics(30, 3)
