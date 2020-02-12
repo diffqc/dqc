1126630d42746715563da88a8775a30e0fc20bfe
mfkasim91 <firman.kasim@gmail.com>
2020-02-12 14:18:13 +0000

    DFT with multiple Hamiltonian

--- ddft/dft/dft.py
+++ ddft/dft/dft.py
@@ -29,6 +29,10 @@ class DFT(torch.nn.Module):
         The external potential (excluding the Kohn-Sham potential).
     * focc: torch.tensor (nbatch, nlowest)
         The occupation factor of the Kohn-Sham orbitals.
+    * hparams: list of torch.tensor (nbatch, ...)
+        List of parameters in the Hamiltonian forward method other than `vext`.
+    * rparams: list of torch.tensor (nbatch, ...)
+        List of parameters used for the overlap operator in the Hamiltonian.
 
     Forward returns
     ---------------
@@ -38,9 +42,6 @@ class DFT(torch.nn.Module):
     """
     def __init__(self, H_model, eks_model, nlowest, **eigen_options):
         super(DFT, self).__init__()
-        eigen_options = set_default_option({
-            "v_init": "randn",
-        }, eigen_options)
         self.H_model = H_model
         self.eks_model = eks_model
         self.vks_model = DifferentialModule(eks_model)
@@ -92,6 +93,116 @@ class DFT(torch.nn.Module):
 
         # calculate the individual non-interacting particles energy
         sum_eigvals = (eigvals * focc).sum(dim=-1) # (nbatch,)
+        vks_integral = self.H_model.integralbox(vks*density, dim=-1)
+
+        # compute the interacting particles energy
+        Etot = sum_eigvals - vks_integral + Eks
+        return Etot
+
+class DFTMulti(torch.nn.Module):
+    """
+    Perform one forward pass of the DFT self-consistent field approach using
+    multiple type of Hamiltonian.
+    Multiple type of Hamiltonian is usually used in radial system where the
+    kinetics term is different for different angular momentums.
+    The density is the sum of all orbitals from the list of Hamiltonian.
+
+    Class arguments
+    ---------------
+    * H_models: list of BaseHamilton
+        List of Hamiltonian transformation.
+    * eks_model: torch.nn.Module
+        Model that calculates the energy density of the Kohn-Sham energy, given
+        the density. The total Kohn-Sham energy is
+        integral(eks_model(density) * dr)
+    * nlowests: list of int
+        Indicates how many Kohn-Sham particles to be retrieved with the lowest
+        energies during the diagonalization process.
+    * **eigen_options: kwargs
+        The options to be passed to the diagonalization algorithm.
+
+    Forward arguments
+    -----------------
+    * density: torch.tensor (nbatch, nr)
+        The density in the spatial grid.
+    * vext: torch.tensor (nbatch, nr)
+        The external potential (excluding the Kohn-Sham potential).
+    * focc: list torch.tensor (nbatch, nlowest)
+        The occupation factor of the Kohn-Sham orbitals.
+    * all_hparams: list of list of torch.tensor (nbatch, ...)
+        List of parameters in the Hamiltonian forward method other than `vext`.
+    * all_rparams: list of list of torch.tensor (nbatch, ...)
+        List of parameters used for the overlap operator in the Hamiltonian.
+
+    Forward returns
+    ---------------
+    * new_density: torch.tensor (nbatch, nr)
+        The new density calculated by one forward pass of the self-consistent
+        field calculation.
+    """
+    def __init__(self, H_models, eks_model, nlowests, **eigen_options):
+        super(DFTMulti, self).__init__()
+        self.H_models = H_models
+        self.eks_model = eks_model
+        self.vks_model = DifferentialModule(eks_model)
+        self.eigen_models = [EigenModule(H_model, nlowest,
+            rlinmodule=H_model.overlap(), **eigen_options) \
+            for (H_model, nlowest) in zip(self.H_models, nlowests)]
+
+    def forward(self, density, vext, foccs, all_hparams=[], all_rparams=[]):
+        # density: (nbatch, nr)
+        # vext: (nbatch, nr)
+        # foccs: list of (nbatch, nlowest)
+
+        # calculate the total potential experienced by Kohn-Sham particles
+        vks = self.vks_model(density) # (nbatch, nr)
+        vext_tot = vext + vks
+
+        # compute the eigenpairs
+        # all_evals: list of (nbatch, nlowest)
+        # all_evecs: list of (nbatch, nr, nlowest)
+        all_eigvals, all_eigvecs = zip([
+            eigen_model((vext_tot, *hparams), rparams=rparams) \
+            for (eigen_model, hparams, rparams) \
+            in zip(self.eigen_models, all_hparams, all_rparams)])
+
+        # normalize the norm of density
+        # dens: list of (nbatch, nr)
+        dens = [(H_model.getdens(eigvecs) * focc.unsqueeze(1)).sum(dim=-1) \
+            for (H_model, eigvecs, focc)\
+            in zip(self.H_models, all_eigvecs, foccs)] # (nbatch, nr, nlowest)
+        new_density = functools.reduce(lambda x,y: x + y, dens)
+
+        # save variables for the post-process calculations
+        self._lc_vks = vks
+        self._lc_vext_tot = vext_tot
+        self._lc_all_eigvals = all_eigvals
+        self._lc_all_eigvecs = all_eigvecs
+        self._lc_density = density
+        self._lc_foccs = foccs
+
+        return new_density
+
+    ############################# post processing #############################
+    def energy(self):
+        # calculate the total potential experienced by Kohn-Sham particles
+        # from the last forward calculation
+        vks = self._lc_vks
+        vext_tot = self._lc_vext_tot
+        all_eigvals = self._lc_all_eigvals
+        all_eigvecs = self._lc_all_eigvecs
+        density = self._lc_density
+        foccs = self._lc_foccs
+
+        # calculates the Kohn-Sham energy
+        eks_density = self.eks_model(density) # energy density (nbatch, nr)
+        Eks = self.H_model.integralbox(eks_density, dim=-1) # (nbatch,)
+
+        # calculate the individual non-interacting particles energy
+        # sum_eigvals_list: list of (nbatch,)
+        sum_eigvals_list = [(eigvals * focc).sum(dim=-1) \
+            for (eigvals, focc) in (all_eigvals, foccs)]
+        sum_eigvals = functools.reduce(lambda x,y: x+y, sum_eigvals_list)
         vks_integral = self.H_model.integralbox(vks*density, dim=-1)
 
         # compute the interacting particles energy
