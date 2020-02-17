823c3fd62a0cdd2e2ddedee23cfe12529ba73d9a
mfkasim91 <firman.kasim@gmail.com>
2020-02-17 10:32:42 +0000

    VKS tests

--- ddft/eks/vks.py
+++ ddft/eks/vks.py
@@ -21,3 +21,36 @@ class VKS(torch.nn.Module):
         dx = torch.autograd.grad(ysum, (xinp,),
             create_graph=grad_enabled)[0]
         return dx # (same shape as x)
+
+if __name__ == "__main__":
+    from ddft.eks.base_eks import BaseEKS
+    from ddft.utils.safeops import safepow
+
+    class EKS1(BaseEKS):
+        def __init__(self, a, p):
+            super(EKS1, self).__init__()
+            self.a = torch.nn.Parameter(a)
+            self.p = torch.nn.Parameter(p)
+
+        def forward(self, x):
+            return self.a * safepow(x, self.p)
+
+    import matplotlib.pyplot as plt
+    from ddft.grids.radialshiftexp import RadialShiftExp
+
+    dtype = torch.float64
+    grid = RadialShiftExp(1e-6, 1e4, 2000, dtype=dtype)
+    rgrid = grid.rgrid
+    density = torch.exp(-rgrid[:,0]).unsqueeze(0)
+
+    a = torch.tensor([1.0]).to(dtype)
+    p = torch.tensor([1.3333]).to(dtype)
+    eks_mdl = EKS1(a, p)
+    vks_mdl = VKS(eks_mdl)
+    eks = eks_mdl(density)
+    vks = vks_mdl(density)
+
+    tonp = lambda x: x.detach().numpy().ravel()
+    plt.plot(tonp(vks))
+    plt.plot(tonp(a * p * density ** (p-1.0)))
+    plt.show()
---
+++ ddft/tests/test_eks.py
@@ -0,0 +1,40 @@
+import torch
+from ddft.eks import BaseEKS, VKS
+from ddft.utils.safeops import safepow
+
+class EKS1(BaseEKS):
+    def __init__(self, a, p):
+        super(EKS1, self).__init__()
+        self.a = torch.nn.Parameter(a)
+        self.p = torch.nn.Parameter(p)
+
+    def forward(self, x):
+        return self.a * x**self.p
+
+def test_vks():
+    dtype = torch.float64
+    grid, density = _setup_density("radialshiftexp", "exp", dtype=dtype)
+
+    a = torch.tensor([1.0]).to(dtype)
+    p = torch.tensor([1.3333]).to(dtype)
+    eks_mdl = EKS1(a, p)
+    vks_mdl = VKS(eks_mdl)
+    eks = eks_mdl(density)
+    vks = vks_mdl(density)
+
+    torch.allclose(eks, a*density**p)
+    torch.allclose(vks, a*p*density**(p-1.0))
+
+def _setup_density(gridname, fcnname, dtype=torch.float64):
+    from ddft.grids.radialshiftexp import RadialShiftExp
+
+    if gridname == "radialshiftexp":
+        grid = RadialShiftExp(1e-6, 1e4, 2000, dtype=dtype)
+    else:
+        raise RuntimeError("Unknown gridname: %s" % gridname)
+    rgrid = grid.rgrid
+    if fcnname == "exp":
+        density = torch.exp(-rgrid[:,0]).unsqueeze(0)
+    else:
+        raise RuntimeError("Unknown fcnname: %s" % fcnname)
+    return grid, density
