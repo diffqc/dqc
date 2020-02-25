707bc8ba9e23126d43611134328588d95414bce1
mfkasim91 <firman.kasim@gmail.com>
2020-02-25 22:31:48 +0000

    Test poisson on radial grid

--- ddft/tests/test_grid.py
+++ ddft/tests/test_grid.py
@@ -15,53 +15,77 @@ def test_radial_integralbox():
     # test if the integral (basis^2) dVolume in the given grid should be
     # equal to 1 where the basis is a pre-computed normalized basis function
     def runtest(gridname, fcnname):
-        grid, rtol = get_radial_grid(gridname, dtype, device)
+        grid = get_radial_grid(gridname, dtype, device)
         prof1 = get_fcn(fcnname, grid.rgrid) # (nr, nbasis)
-        runtest_integralbox(grid, rtol, prof1)
+        rtol, atol = get_rtol_atol("integralbox", gridname)
+        runtest_integralbox(grid, prof1, rtol=rtol, atol=atol)
 
     for gridname, fcnname in product(radial_gridnames, radial_fcnnames):
         runtest(gridname, fcnname)
 
 def test_spherical_integralbox():
     def runtest(spgridname, radgridname, fcnname):
-        radgrid, rtol = get_radial_grid(radgridname, dtype, device)
+        radgrid = get_radial_grid(radgridname, dtype, device)
         sphgrid = get_spherical_grid(spgridname, radgrid, dtype, device)
         prof1 = get_fcn(fcnname, sphgrid.rgrid)
-        runtest_integralbox(sphgrid, rtol, prof1)
+        rtol, atol = get_rtol_atol("integralbox", spgridname, radgridname)
+        runtest_integralbox(sphgrid, prof1, rtol=rtol, atol=atol)
 
     for gridname, radgridname, fcnname in product(sph_gridnames, radial_gridnames, radial_fcnnames):
         runtest(gridname, radgridname, fcnname)
 
-# def test_radial_poisson():
-#     def runtest(gridname, fcnname):
-#         grid, rtol = get_radial_grid(gridname, dtype, device)
-#         prof1 = get_fcn(fcnname, grid.rgrid)
-#         poisson1 = get_poisson(fcnname, grid.rgrid)
-#         runtest_poisson(grid, rtol, prof1, poisson1)
-#
-#     for gridname, fcnname in product(radial_gridnames, radial_fcnnames):
-#         runtest(gridname, fcnname)
+def test_radial_poisson():
+    def runtest(gridname, fcnname):
+        grid = get_radial_grid(gridname, dtype, device)
+        prof1 = get_fcn(fcnname, grid.rgrid)
+        poisson1 = get_poisson(fcnname, grid.rgrid)
+        rtol, atol = get_rtol_atol("poisson", gridname)
+        runtest_poisson(grid, prof1, poisson1, rtol=rtol, atol=atol)
+
+    for gridname, fcnname in product(radial_gridnames, radial_fcnnames):
+        runtest(gridname, fcnname)
 
 ############################## helper functions ##############################
-def runtest_integralbox(grid, rtol, prof):
+def runtest_integralbox(grid, prof, rtol, atol):
     ones = torch.tensor([1.0], dtype=prof.dtype, device=prof.device)
     int1 = grid.integralbox(prof*prof, dim=0)
-    assert torch.allclose(int1, ones, rtol=rtol, atol=0.0)
+    assert torch.allclose(int1, ones, rtol=rtol, atol=atol)
 
-def runtest_poisson(grid, rtol, prof, poisson):
+def runtest_poisson(grid, prof, poisson, rtol, atol):
     pois = grid.solve_poisson(prof.transpose(-2,-1)).transpose(-2,-1)
-    assert torch.allclose(pois, poisson, rtol=rtol)
+    pois = pois - pois[-1:,:]
+    poisson = poisson - poisson[-1:,:]
+    assert torch.allclose(pois/pois.abs().max(), poisson/poisson.abs().max(), rtol=rtol, atol=atol)
+
+def get_rtol_atol(taskname, gridname1, gridname2=None):
+    rtolatol = {
+        "integralbox": {
+            # this is compared to 1, so rtol has the same effect as atol
+            "radialshiftexp": [1e-4, 0.0],
+            "legradialshiftexp": [1e-8, 0.0],
+            "lebedev": {
+                "radialshiftexp": [1e-4, 0.0],
+                "legradialshiftexp": [1e-8, 0.0],
+            }
+        },
+        "poisson": {
+            "radialshiftexp": [0.0, 2e-4],
+            "legradialshiftexp": [0.0, 8e-5],
+        }
+    }
+    if gridname2 is None:
+        return rtolatol[taskname][gridname1]
+    else:
+        return rtolatol[taskname][gridname1][gridname2]
 
 def get_radial_grid(gridname, dtype, device):
     if gridname == "radialshiftexp":
         grid = RadialShiftExp(1e-6, 1e4, 2000, dtype=dtype, device=device)
-        rtol = 1e-4
     elif gridname == "legradialshiftexp":
         grid = LegendreRadialShiftExp(1e-6, 1e4, 200, dtype=dtype, device=device)
-        rtol = 1e-8
     else:
         raise RuntimeError("Unknown radial grid name: %s" % gridname)
-    return grid, rtol
+    return grid
 
 def get_spherical_grid(gridname, radgrid, dtype, device):
     if gridname == "lebedev":
@@ -76,7 +100,7 @@ def get_fcn(fcnname, rgrid):
 
     if fcnname in ["gauss1", "exp1"]:
         rs = rgrid[:,0].unsqueeze(-1) # (nr,1)
-        gw = torch.logspace(np.log10(1e-4), np.log10(1e2), 100).to(dtype).to(device)
+        gw = torch.logspace(np.log10(1e-4), np.log10(1e0), 30).to(dtype).to(device)
         if fcnname == "gauss1":
             unnorm_basis = torch.exp(-rs*rs / (2*gw*gw)) * rs # (nr,ng)
             norm = np.sqrt(2./3) / gw**2.5 / np.pi**.75 # (ng)
@@ -89,18 +113,17 @@ def get_fcn(fcnname, rgrid):
     raise RuntimeError("Unknown function name: %s" % fcnname)
 
 def get_poisson(fcnname, rgrid):
-    # ???
     dtype = rgrid.dtype
     device = rgrid.device
 
     if fcnname in ["gauss1", "exp1"]:
         rs = rgrid[:,0].unsqueeze(-1) # (nr,1)
-        gw = torch.logspace(np.log10(1e-4), np.log10(1e2), 100).to(dtype).to(device)
+        gw = torch.logspace(np.log10(1e-4), np.log10(1e0), 30).to(dtype).to(device)
         if fcnname == "gauss1":
-            unnorm_basis = torch.exp(-rs*rs / (2*gw*gw)) * rs # (nr,ng)
-            norm = np.sqrt(2./3) / gw**2.5 / np.pi**.75 # (ng)
-            return unnorm_basis * norm # (nr, ng)
+            rg = rs/(np.sqrt(2)*gw)
+            sqrtpi = np.sqrt(np.pi)
+            y = -torch.sqrt(gw) * ((2*np.sqrt(2)*(1-torch.exp(-rg*rg))*gw + rs*sqrtpi) - rs*sqrtpi*torch.erf(rg)) / (np.sqrt(3)*rs*np.pi**.75)
+            return y # (nr, ng)
         elif fcnname == "exp1":
-            unnorm_basis = torch.exp(-rs/gw)
-            norm = 1./torch.sqrt(np.pi*gw**3)
-            return unnorm_basis * norm
+            y = -gw*gw*(2*gw - torch.exp(-rs/gw)*(rs+2*gw)) / (rs*np.sqrt(np.pi)*gw**1.5)
+            return y
