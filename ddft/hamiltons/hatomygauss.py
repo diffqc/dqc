9ea707dad1c4fc3bd035d0b1e8a29cf56a76802b
mfkasim91 <firman.kasim@gmail.com>
2020-02-28 14:47:05 +0000

    Add small noise for different angular momentum to break the degeneracy

--- ddft/hamiltons/hatomygauss.py
+++ ddft/hamiltons/hatomygauss.py
@@ -113,11 +113,12 @@ class HamiltonAtomYGauss(BaseHamilton):
         # create the angular momentum factor
         lhat = []
         eps = 1e-9
+        eps2 = 1e-8
         for angmom in range(maxangmom+1):
             for j in range(-angmom, angmom+1):
                 # small noise epsilon to avoid degeneracy and improve the
                 # numerical stability in avoiding complex eigenvalues
-                noise = j * eps
+                noise = j * eps + angmom * eps2
                 lhat.append(angmom*(angmom+1)+noise)
             # lhat = lhat + [angmom*(angmom+1)]*(2*angmom+1)
         self.lhat = torch.tensor(lhat, dtype=dtype, device=device) # (nsh,)
