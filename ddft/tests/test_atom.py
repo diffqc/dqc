83977c5d57ef9120cfcbb2e8d6944ef121d1692c
mfkasim91 <firman.kasim@gmail.com>
2020-03-03 11:23:22 +0000

    Update test_atom

--- ddft/tests/test_atom.py
+++ ddft/tests/test_atom.py
@@ -4,10 +4,13 @@ from ddft.api.atom import atom
 def test_atom():
     dtype = torch.float64
     energies = {
-        1: -0.4066,
-        2: -2.7238,
+        1: -0.4063,
+        2: -2.7221,
     }
 
     for atomz, ene in energies.items():
-        energy, density = atom(atomz, eks_model="lda", dtype=dtype)
+        energy, density = atom(atomz, eks_model="lda",
+                 gwmin=1e-5, gwmax=1e3, ng=60,
+                 rmin=1e-6, rmax=1e2, nr=200,
+                 dtype=dtype)
         assert torch.allclose(energy, torch.tensor([ene], dtype=dtype), atol=1e-4)
