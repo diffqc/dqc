07b02a09944cc53f125c2235a4772e82d0180259
mfkasim91 <firman.kasim@gmail.com>
2020-03-12 16:46:54 +0000

    Test VKS for multiatomgrid

--- ddft/tests/test_eks.py
+++ ddft/tests/test_eks.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from ddft.eks import BaseEKS, VKS, Hartree
 from ddft.utils.safeops import safepow
+from ddft.grids.base_grid import BaseRadialAngularGrid, Base3DGrid
 
 class EKS1(BaseEKS):
     def __init__(self, a, p):
@@ -20,6 +21,10 @@ def test_vks_lebedev():
     run_vks_test("lebedev", "gauss-l2")
     run_vks_test("lebedev", "gauss-l1m1")
     run_vks_test("lebedev", "gauss-l2m2")
+
+def test_vks_becke():
+    run_vks_test("becke", "exp")
+    run_vks_test("becke", "exp-twocentres")
 
 def test_hartree_radial_legendre():
     run_hartree_test("legradialshiftexp", "exp")
@@ -62,20 +67,40 @@ def run_hartree_test(gridname, fcnname, rtol=1e-5, atol=1e-8):
 def _setup_density(gridname, fcnname, dtype=torch.float64):
     from ddft.grids.radialgrid import LegendreRadialShiftExp
     from ddft.grids.sphangulargrid import Lebedev
+    from ddft.grids.multiatomsgrid import BeckeMultiGrid
 
     if gridname == "legradialshiftexp":
         grid = LegendreRadialShiftExp(1e-6, 1e4, 200, dtype=dtype)
     elif gridname == "lebedev":
         radgrid = LegendreRadialShiftExp(1e-6, 1e4, 200, dtype=dtype)
         grid = Lebedev(radgrid, prec=13, basis_maxangmom=3, dtype=dtype)
+    elif gridname == "becke":
+        atompos = torch.tensor([[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]], dtype=dtype) # (natom, ndim)
+        radgrid = LegendreRadialShiftExp(1e-6, 1e2, 200, dtype=dtype)
+        sphgrid = Lebedev(radgrid, prec=13, basis_maxangmom=8, dtype=dtype)
+        grid = BeckeMultiGrid(sphgrid, atompos, dtype=dtype)
     else:
         raise RuntimeError("Unknown gridname: %s" % gridname)
 
-    rgrid = grid.rgrid
-    rs = rgrid[:,0]
+    rgrid = grid.rgrid # (nr, ndim)
+    if rgrid.shape[1] == 1:
+        rs = rgrid[:,0]
     if rgrid.shape[1] == 3:
-        phi = rgrid[:,1]
-        theta = rgrid[:,2]
+        if isinstance(grid, Base3DGrid):
+            xyzgrid = grid.rgrid_in_xyz # (nr, ndim)
+        if isinstance(grid, BaseRadialAngularGrid):
+            rs = rgrid[:,0]
+            phi = rgrid[:,1]
+            theta = rgrid[:,2]
+        else:
+            x = rgrid[:,0]
+            y = rgrid[:,1]
+            z = rgrid[:,2]
+            rs = rgrid.norm(dim=-1) # (nr,)
+            xy = rgrid[:,:2].norm(dim=-1)
+            phi = torch.atan2(y, x)
+            theta = torch.atan2(xy, z)
+
     if fcnname == "exp":
         density = torch.exp(-rs)
     elif fcnname == "gauss-l1":
@@ -86,6 +111,9 @@ def _setup_density(gridname, fcnname, dtype=torch.float64):
         density = torch.exp(-rs*rs/2) * torch.sin(theta) * torch.cos(phi)
     elif fcnname == "gauss-l2m2":
         density = torch.exp(-rs*rs/2) * 3*torch.sin(theta)**2 * torch.cos(2*phi) # (nr,1)
+    elif fcnname == "exp-twocentres":
+        dist = (xyzgrid - atompos.unsqueeze(1)).norm(dim=-1) # (natom, nr)
+        density = torch.exp(-dist).sum(dim=0)
     else:
         raise RuntimeError("Unknown fcnname: %s" % fcnname)
 
