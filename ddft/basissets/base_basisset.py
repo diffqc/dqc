4e04b572f014382aecbbcbbf7d00969c90c7ad40
mfkasim91 <firman.kasim@gmail.com>
2020-03-19 16:11:05 +0000

    Tidying up the code regarding base expansion

--- ddft/basissets/base_basisset.py
+++ ddft/basissets/base_basisset.py
@@ -38,45 +38,42 @@ def expand_basis(spdfs, all_alphas, all_coeffs, cartesian):
         coeffs = all_coeffs[icgto]
         nelmts = len(alphas)
 
-        if spdf == "s":
-            ijks += [ ([(0,0,0)]*nelmts) ]
-            new_alphas += [alphas]
-            new_coeffs += [coeffs]
-        elif spdf == "p":
-            ijks += [([(0,0,1)]*nelmts)] + \
-                    [([(0,1,0)]*nelmts)] + \
-                    [([(1,0,0)]*nelmts)]
-            new_alphas += [alphas] * 3
-            new_coeffs += [coeffs] * 3
-        elif spdf == "d":
-            ijks += [([(0,0,2)]*nelmts)] + \
-                    [([(0,2,0)]*nelmts)] + \
-                    [([(2,0,0)]*nelmts)] + \
-                    [([(1,1,0)]*nelmts)] + \
-                    [([(1,0,1)]*nelmts)] + \
-                    [([(0,1,1)]*nelmts)]
-            new_alphas += [alphas]*6
-            new_coeffs += [coeffs]*6
-        elif spdf == "f":
-            ijks += [([(0,0,3)]*nelmts)] + \
-                    [([(0,3,0)]*nelmts)] + \
-                    [([(3,0,0)]*nelmts)] + \
-                    [([(2,1,0)]*nelmts)] + \
-                    [([(1,2,0)]*nelmts)] + \
-                    [([(2,0,1)]*nelmts)] + \
-                    [([(1,0,2)]*nelmts)] + \
-                    [([(0,2,1)]*nelmts)] + \
-                    [([(0,1,2)]*nelmts)] + \
-                    [([(1,1,1)]*nelmts)]
-            new_alphas += [alphas]*10
-            new_coeffs += [coeffs]*10
-        elif spdf == "sp":
-            ijks += [([(0,0,0), (0,0,1)]*(nelmts//2))] + \
-                    [([(0,0,0), (0,1,0)]*(nelmts//2))] + \
-                    [([(0,0,0), (1,0,0)]*(nelmts//2))]
-            new_alphas += [alphas]*3
-            new_coeffs += [coeffs]*3
+        nrep, ijklist = get_angmom_spec(spdf, cartesian)
+        new_alphas += [alphas] * nrep
+        new_coeffs += [coeffs] * nrep
+        for ijk in ijklist:
+            ijks += [(ijk * (nelmts//len(ijk)))]
+
     return ijks, new_alphas, new_coeffs
+
+def get_angmom_spec(spdf, cartesian):
+    nrep = {
+        "s": [1,1],
+        "p": [3,3],
+        "sp": [3,3],
+        "d": [5,6],
+        "f": [7,10],
+        "g": [9,15],
+        "h": [11,21],
+    }[spdf][1 if cartesian else 0]
+    if cartesian:
+        ijk_list = {
+            "s": [[(0,0,0)]],
+            "p": [[(0,0,1)], [(0,1,0)], [(1,0,0)]],
+            "d": [[(0,0,2)], [(0,2,0)], [(2,0,0)], [(1,1,0)], [(1,0,1)], [(0,1,1)]],
+            "f": [[(0,0,3)], [(0,3,0)], [(3,0,0)], [(2,1,0)], [(1,2,0)], [(2,0,1)],
+                  [(1,0,2)], [(0,2,1)], [(0,1,2)], [(1,1,1)]],
+            "sp": [[(0,0,0), (0,0,1)], [(0,0,0), (0,1,0)], [(0,0,0), (1,0,0)]],
+        }[spdf]
+    else:
+        ijk_list = {
+            "s": [[(0,0)]],
+            "p": [[(1,0)], [(1,-1)], [(1,1)]],
+            "d": [[(2,0)], [(2,-1)], [(2,1)], [(2,-2)], [(2,2)]],
+            "f": [[(3,0)], [(3,-1)], [(3,1)], [(3,-2)], [(3,2)], [(3,-3)], [(3,3)]],
+            "sp": [[(0,0),(1,0)], [(0,0),(1,-1)], [(0,0),(1,1)]],
+        }[spdf]
+    return nrep, ijk_list
 
 def flatten(array2d):
     res = []
