8bb363cd2be767845cf8d3223d4d64c206663b01
mfkasim91 <firman.kasim@gmail.com>
2020-04-02 15:38:27 +0100

    jitscript the spharmonics function

--- ddft/utils/spharmonics.py
+++ ddft/utils/spharmonics.py
@@ -2,24 +2,27 @@ import torch
 import numpy as np
 from ddft.utils.legendre import legval, assoclegval, deriv_assoclegval, deriv_assoclegval_azimuth
 
-def spharmonics(costheta, phi, maxangmom, onlytheta=False):
+@torch.jit.script
+def spharmonics(costheta:torch.Tensor, phi:torch.Tensor,
+                maxangmom:int, onlytheta:int=False) -> torch.Tensor:
     # costheta: (nphitheta,)
     # phi: (nphitheta,)
     # maxangmom: int
     # return: (nsh, phitheta)
 
-    nsh = (maxangmom+1)**2 if not onlytheta else (maxangmom+1)
+    nsh = (maxangmom+1)*(maxangmom+1) if not onlytheta else (maxangmom+1)
     nphitheta = phi.shape[0]
 
     dtype = phi.dtype
     device = phi.device
-    angbasis = torch.empty((nsh, nphitheta), dtype=dtype, device=device)
+    size = (int(nsh), int(nphitheta))
+    angbasis = torch.empty(size, dtype=dtype, device=device)
     angbasis_row = 0
     for l in range(maxangmom+1):
         legcostheta = legval(costheta, l) # (nphitheta,)
 
         # m = 0
-        normm0 = np.sqrt(2*l+1)
+        normm0 = (2*l+1)**0.5
         angbasis[angbasis_row] = legcostheta * normm0
         angbasis_row += 1
 
@@ -29,7 +32,7 @@ def spharmonics(costheta, phi, maxangmom, onlytheta=False):
             for m in range(1,l+1):
                 alegcostheta = assoclegval(costheta, l, m)
                 nm = nm * (l-m+1) * (l+m)
-                norm = normm0 / np.sqrt(nm)
+                norm = normm0 / nm**0.5
                 angbasis[angbasis_row] = alegcostheta * torch.cos(m*phi) * norm
                 angbasis_row += 1
                 angbasis[angbasis_row] = alegcostheta * torch.sin(m*phi) * norm
