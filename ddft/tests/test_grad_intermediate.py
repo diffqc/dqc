967dc477d5c8fcf0b28bc6d1b1a7356ee4b6b0ee
mfkasim91 <firman.kasim@gmail.com>
2020-04-03 17:30:01 +0100

    Combine the intermediate level methods gradient tests

--- ddft/tests/test_grad_intermediate.py
+++ ddft/tests/test_grad_intermediate.py
@@ -1,15 +1,50 @@
 import torch
 from torch.autograd import gradcheck, gradgradcheck
-from ddft.dft.dft import DFT
 from ddft.eks.base_eks import BaseEKS
 from ddft.eks.hartree import Hartree
+from ddft.dft.dft import DFT
 from ddft.basissets.cgto_basis import CGTOBasis
 from ddft.grids.radialgrid import LegendreRadialShiftExp
 from ddft.grids.sphangulargrid import Lebedev
 from ddft.grids.multiatomsgrid import BeckeMultiGrid
 from ddft.utils.safeops import safepow
 
+"""
+Test the gradient for the intermediate methods (not basic module and not API)
+"""
+
 dtype = torch.float64
+
+def test_grad_basis_cgto():
+    basisname = "6-311++G**"
+    ns0 = 7
+
+    rmin = 1e-5
+    rmax = 1e2
+    nr = 100
+    prec = 13
+    nrgrid = 148 * nr
+
+    def fcn(atomzs, atomposs, wf, vext):
+        radgrid = LegendreRadialShiftExp(rmin, rmax, nr, dtype=dtype)
+        sphgrid = Lebedev(radgrid, prec=prec, basis_maxangmom=4, dtype=dtype)
+        grid = BeckeMultiGrid(sphgrid, atomposs, dtype=dtype)
+        basis = CGTOBasis(basisname, cartesian=True)
+        basis.construct_basis(atomzs, atomposs, requires_grad=False)
+        H_model = basis.get_hamiltonian(grid)
+        # print(H_model.rgrid.shape)
+        # print(H_model.shape)
+        y = H_model(wf, vext)
+        return (y**2).sum()
+
+    atomzs = torch.tensor([1.0, 1.0], dtype=dtype)
+    atomposs = torch.tensor([[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]], dtype=dtype).requires_grad_()
+    ns = ns0 * len(atomzs)
+    wf = torch.ones((1,ns,1), dtype=dtype)
+    vext = torch.zeros((1,nrgrid), dtype=dtype)
+
+    gradcheck(fcn, (atomzs, atomposs, wf, vext))
+    gradgradcheck(fcn, (atomzs, atomposs, wf, vext))
 
 def test_grad_dft_cgto():
     basisname = "6-311++G**"
@@ -60,4 +95,5 @@ def test_grad_dft_cgto():
     gradcheck(fcn, (atomzs, atomposs, a, p, "energy"))
     gradcheck(fcn, (atomzs, atomposs, a, p, "density"))
     gradgradcheck(fcn, (atomzs, atomposs, a, p, "energy"))
+    # choosing smaller eps make the numerical method produces nan, don't know why
     gradgradcheck(fcn, (atomzs, atomposs, a, p, "density"), eps=1e-3)
