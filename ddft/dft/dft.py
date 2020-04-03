Fix the bug in calculating the gradient w.r.t. density

ddft/dft/dft.py
@@ -75,7 +75,6 @@ class DFT(torch.nn.Module):
         self._lc_eigvals = eigvals
         self._lc_eigvecs = eigvecs
         self._lc_density = density
-        self._lc_new_density = new_density
         self._lc_focc = focc
         # parameters that would be computed later
         self._lc_energy = None
@@ -84,7 +83,7 @@ class DFT(torch.nn.Module):
 
     ############################# post processing #############################
     def density(self):
-        return self._lc_new_density
+        return self._lc_density
 
     def energy(self):
         # calculate the total potential experienced by Kohn-Sham particles
@@ -196,7 +195,6 @@ class DFTMulti(torch.nn.Module):
         self._lc_all_eigvals = all_eigvals
         self._lc_all_eigvecs = all_eigvecs
         self._lc_density = density
-        self._lc_new_density = new_density
         self._lc_foccs = foccs
         # parameters that would be computed later
         self._lc_energy = None
@@ -205,7 +203,7 @@ class DFTMulti(torch.nn.Module):
 
     ############################# post processing #############################
     def density(self):
-        return self._lc_new_density
+        return self._lc_density
 
     def energy(self):
         # calculate the total potential experienced by Kohn-Sham particles