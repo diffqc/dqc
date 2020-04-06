403fd663515d607de596c54570b47eaa3e73d841
mfkasim91 <firman.kasim@gmail.com>
2020-04-06 14:37:46 +0100

    Use grad and gradgradcheck in test equilibrium

--- ddft/tests/test_equilibrium.py
+++ ddft/tests/test_equilibrium.py
@@ -1,6 +1,7 @@
 import time
 import random
 import torch
+from torch.autograd import gradcheck, gradgradcheck
 from ddft.utils.fd import finite_differences
 from ddft.tests.utils import compare_grad_with_fd
 from ddft.modules.equilibrium import EquilibriumModule
@@ -9,7 +10,7 @@ def test_equil_1():
     class DummyModule(torch.nn.Module):
         def __init__(self, A):
             super(DummyModule, self).__init__()
-            self.A = torch.nn.Parameter(A)
+            self.A = A
 
         def forward(self, y, x):
             # y: (nbatch, nr)
@@ -27,39 +28,21 @@ def test_equil_1():
     dtype = torch.float64
     nr = 7
     nbatch = 1
-    A  = torch.randn((nr, nr)).to(dtype)
+    A  = torch.randn((nr, nr)).to(dtype).requires_grad_()
     x  = torch.rand((nbatch, nr)).to(dtype).requires_grad_()
-    y0 = torch.rand((nbatch, nr)).to(dtype).requires_grad_()
+    y0 = torch.rand((nbatch, nr)).to(dtype)
 
     model = DummyModule(A)
     eqmodel = EquilibriumModule(model)
     y = eqmodel(y0, x)
-
-    print("Forward results:")
-    print(y)
-    print(model(y, x))
-    print("    should be close to 1:")
-    print(y / model(y, x))
+    assert torch.allclose(y, model(y, x))
 
     def getloss(A, x, y0, return_model=False):
         model = DummyModule(A)
         eqmodel = EquilibriumModule(model)
         y = eqmodel(y0, x)
         loss = (y*y).sum()
-        if not return_model:
-            return loss
-        else:
-            return loss, model
+        return loss
 
-    compare_grad_with_fd(getloss, (A, x, y0), [1], eps=1e-5, rtol=1e-3)
-
-    # check A_grad manually because it is a parameter
-    # gradient with backprop
-    loss, model = getloss(A, x, y0, return_model=True)
-    x.grad.zero_()
-    loss.backward()
-    A_grad = list(model.parameters())[0].grad.data
-    # gradient with fd
-    A_fd = finite_differences(getloss, (A, x, y0), 0, eps=1e-5)
-    ratio = A_grad / A_fd
-    assert torch.allclose(ratio, torch.ones_like(ratio), rtol=3e-3)
+    gradcheck(getloss, (A, x, y0))
+    # gradgradcheck(getloss, (A, x, y0))
