bc9d47a9e5bc625f49c437feb47894cc37df09e3
mfkasim91 <firman.kasim@gmail.com>
2020-04-30 18:15:26 +0100

    Safer safepow

--- ddft/utils/safeops.py
+++ ddft/utils/safeops.py
@@ -1,26 +1,7 @@
 import torch
 
-def safepow(a, p, eps=1e-9):
-    return _safepow.apply(a, p, eps)
-
-class _safepow(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, a, p, eps):
-        ctx.a = a
-        ctx.p = p
-        ctx.res = a**p
-        ctx.eps = eps
-        return ctx.res
-
-    @staticmethod
-    def backward(ctx, grad_res):
-        a = ctx.a.clone()
-        a[a.abs() < ctx.eps] = ctx.eps
-        gr = grad_res * ctx.res
-        grad_a = None
-        grad_p = None
-        if isinstance(ctx.a, torch.Tensor):
-            grad_a = gr * ctx.p / a
-        if isinstance(ctx.p, torch.Tensor):
-            grad_p = gr * torch.log(a)
-        return (grad_a, grad_p, None)
+def safepow(a, p, eps=1e-12):
+    if torch.any(a < 0):
+        raise RuntimeError("safepow only works for positive base")
+    base = torch.sqrt(a*a + eps*eps) # soft clip
+    return base**p
