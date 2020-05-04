cf096a3d1acbcc8e342d236acae6a3a7215f4fcc
mfkasim91 <firman.kasim@gmail.com>
2020-05-04 13:27:41 +0100

    Test molecule for H2 vibration

--- ddft/tests/test_molecule.py
+++ ddft/tests/test_molecule.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.autograd import gradcheck, gradgradcheck
 from ddft.api.molecule import molecule
@@ -21,10 +22,11 @@ class PseudoLDA(BaseEKS):
         self.a, self.p = params[:2]
         return 2
 
-def get_molecule(molname, with_energy=False):
+def get_molecule(molname, distance=1.0, with_energy=False):
     if molname == "H2":
         atomzs = torch.tensor([1.0, 1.0], dtype=dtype)
-        atomposs = torch.tensor([[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]], dtype=dtype).requires_grad_()
+        atomposs = distance * torch.tensor([[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]], dtype=dtype)
+        atomposs = atomposs.requires_grad_()
         energy = torch.tensor(-0.976186, dtype=dtype) # only works for LDA
     else:
         raise RuntimeError("Unknown molecule %s" % molname)
@@ -61,3 +63,51 @@ def test_mol_grad():
     gradcheck(get_energy, (a, p, atomzs, atomposs, "density"))
     # gradgradcheck(get_energy, (a, p, atomzs, atomposs, "energy"))
     # gradgradcheck(get_energy, (a, p, atomzs, atomposs, False))
+
+def test_h2_vibration():
+    def get_energy(a, p, dist):
+        bck_options = {
+            "max_niter": 100,
+        }
+        atomzs, atomposs = get_molecule("H2", distance=dist)
+        eks_model = PseudoLDA(a, p)
+        energy, density = molecule(atomzs, atomposs, eks_model=eks_model,
+            bck_options=bck_options)
+        return energy
+
+    # pseudo-lda eks model
+    a = torch.tensor([-0.7385587663820223]).to(dtype).requires_grad_()
+    p = torch.tensor([4./3]).to(dtype).requires_grad_()
+
+    dists = torch.tensor(
+        [1.475, 1.4775, 1.48, 1.481, 1.4825, 1.485],
+        dtype=dtype)
+
+    energies = [float(get_energy(a, p, dist).view(-1)[0]) for dist in dists]
+
+    # get the minimum index
+    min_idx = 0
+    min_ene = energies[0]
+    for i in range(1,len(energies)):
+        if energies[i] < min_ene:
+            min_ene = energies[i]
+            min_idx = i
+
+    # fit the square curve
+    dists_np = np.asarray(dists)
+    energies_np = np.asarray(energies)
+    num_k = np.polyfit(dists_np, energies_np, 2)[0] * 2
+
+    # get the vibration frequency
+    min_dist = dists[min_idx].requires_grad_()
+    min_energy = get_energy(a, p, min_dist)
+    deds = torch.autograd.grad(min_energy, min_dist, create_graph=True)
+    ana_k = torch.autograd.grad(deds, min_dist)
+
+    print("Numerical: %.8e" % num_k)
+    print("Analytical: %.8e" % ana_k)
+
+    assert np.abs(num_k-ana_k)/num_k < 1e-2
+
+if __name__ == "__main__":
+    vibration_h2()
