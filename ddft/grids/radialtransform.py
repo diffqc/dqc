24984ccece241e1aec4f6b48d690629c090714a3
mfkasim91 <firman.kasim@gmail.com>
2020-05-12 16:14:15 +0100

    Double exp2 transform

--- ddft/grids/radialtransform.py
+++ ddft/grids/radialtransform.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import lintorch as lt
 from ddft.grids.base_grid import BaseTransformed1DGrid
 
 class ShiftExp(BaseTransformed1DGrid):
@@ -38,3 +39,40 @@ class ShiftExp(BaseTransformed1DGrid):
         else:
             raise RuntimeError("Unimplemented %s method for setparams" % methodname)
 
+class DoubleExp2(BaseTransformed1DGrid):
+    def __init__(self, alpha, rmin, rmax, nr, dtype=torch.float, device=torch.device('cpu')):
+        # setup the parameters needed for the transformation
+        self.alpha = alpha
+        self.xmin = self.invtransform(rmin)
+        self.xmax = self.invtransform(rmax)
+        self.x = torch.linspace(self.xmin, self.xmax, nr, dtype=dtype, device=device)
+
+    def transform(self, xlg):
+        x = (xlg+1)*0.5 * (self.xmax - self.xmin) + self.xmin
+        return torch.exp(self.alpha*x - torch.exp(-x))
+
+    def invtransform(self, rs):
+        logrs = torch.log(rs)
+        def iter_fcn(x, logrs, inv_alpha):
+            return inv_alpha * (logrs + torch.exp(-x))
+        x0 = torch.zeros_like(rs).to(rs.device)
+        x = lt.equilibrium(iter_fcn, x0, params=[logrs, 1./self.alpha])
+        return x
+
+    def get_scaling(self, rs):
+        x = self.invtransform(rs)
+        return rs * (self.alpha - torch.exp(-x))
+
+    #################### editable module parts ####################
+    def getparams(self, methodname):
+        if methodname == "invtransform" or methodname == "transform" or methodname == "get_scaling":
+            return [self.alpha]
+        else:
+            raise RuntimeError("Unimplemented %s method for getparams" % methodname)
+
+    def setparams(self, methodname, *params):
+        if methodname == "invtransform" or methodname == "transform" or methodname == "get_scaling":
+            self.alpha, = params[:1]
+            return 1
+        else:
+            raise RuntimeError("Unimplemented %s method for setparams" % methodname)
