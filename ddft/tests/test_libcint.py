755dcd8a5301ecbcfdb956272e2ec991a931935a
mfkasim1 <firman.kasim@gmail.com>
2020-11-28 18:10:38 +0000

    Refactoring the test libcint

--- ddft/tests/test_libcint.py
+++ ddft/tests/test_libcint.py
@@ -1,8 +1,31 @@
+from collections import namedtuple
 import torch
 import pytest
 from ddft.basissets.cgtobasis import loadbasis
 from ddft.hamiltons.libcintwrapper import LibcintWrapper
 from ddft.basissets.cgtobasis import AtomCGTOBasis
+
+AtomEnv = namedtuple("AtomEnv", ["poss", "basis", "rgrid", "atomzs"])
+
+def get_atom_env(dtype, rgrid=False):
+    pos1 = torch.tensor([0.0, 0.0,  0.8], dtype=dtype, requires_grad=True)
+    pos2 = torch.tensor([0.0, 0.0, -0.8], dtype=dtype, requires_grad=True)
+    poss = [pos1, pos2]
+    atomzs = [1, 1]
+    basis = "3-21G"
+
+    # set the grid
+    n = 3
+    z = torch.linspace(-5, 5, n, dtype=dtype)
+    zeros = torch.zeros(n, dtype=dtype)
+    rgrid = torch.cat((zeros[None, :], zeros[None, :], z[None, :]), dim=0).T.contiguous().to(dtype)
+
+    return AtomEnv(
+        poss = poss,
+        basis = basis,
+        rgrid = rgrid,
+        atomzs = atomzs
+    )
 
 @pytest.mark.parametrize(
     "int_type",
@@ -10,16 +33,19 @@ from ddft.basissets.cgtobasis import AtomCGTOBasis
 )
 def test_integral_grad(int_type):
     dtype = torch.double
-    pos1 = torch.tensor([0.0, 0.0,  0.8], dtype=dtype, requires_grad=True)
-    pos2 = torch.tensor([0.0, 0.0, -0.8], dtype=dtype, requires_grad=True)
 
-    basis = "3-21G"
+    atomenv = get_atom_env(dtype)
+    pos1 = atomenv.poss[0]
+    pos2 = atomenv.poss[1]
+    allbases = [
+        loadbasis("%d:%s" % (atomz, atomenv.basis), dtype=dtype, requires_grad=False) \
+        for atomz in atomenv.atomzs
+    ]
 
     def get_int1e(pos1, pos2, name):
-        bases = loadbasis("1:%s" % basis, dtype=dtype, requires_grad=False)
-        atombasis1 = AtomCGTOBasis(atomz=1, bases=bases, pos=pos1)
-        atombasis2 = AtomCGTOBasis(atomz=1, bases=bases, pos=pos2)
-        env = LibcintWrapper([atombasis1, atombasis2], spherical=False)
+        atombasis1 = AtomCGTOBasis(atomz=atomenv.atomzs[0], bases=allbases[0], pos=pos1)
+        atombasis2 = AtomCGTOBasis(atomz=atomenv.atomzs[1], bases=allbases[1], pos=pos2)
+        env = LibcintWrapper([atombasis1, atombasis2], spherical=True)
         if name == "overlap":
             return env.overlap()
         elif name == "kinetic":
@@ -35,29 +61,34 @@ def test_integral_grad(int_type):
     torch.autograd.gradcheck(get_int1e, (pos1, pos2, int_type))
     torch.autograd.gradgradcheck(get_int1e, (pos1, pos2, int_type))
 
+# TODO: complete this
+# @pytest.mark.parametrize(
+#     "eval_type",
+#     ["", "grad"]
+# )
+# def test_eval_vs_pyscf(eval_type):
+#     pass
+
 @pytest.mark.parametrize(
     "eval_type",
     ["", "grad", "lapl"]
 )
 def test_eval_grad(eval_type):
     dtype = torch.double
-    pos1 = torch.tensor([0.0, 0.0,  0.8], dtype=dtype, requires_grad=True)
-    pos2 = torch.tensor([0.0, 0.0, -0.8], dtype=dtype, requires_grad=True)
 
-    basis = "3-21G"
-
-    # set the grid
-    gradcheck = True
-    n = 3 if gradcheck else 1000
-    z = torch.linspace(-5, 5, n, dtype=dtype)
-    zeros = torch.zeros(n, dtype=dtype)
-    rgrid = torch.cat((zeros[None, :], zeros[None, :], z[None, :]), dim=0).T.contiguous().to(dtype)
+    atomenv = get_atom_env(dtype, rgrid=True)
+    pos1 = atomenv.poss[0]
+    pos2 = atomenv.poss[1]
+    allbases = [
+        loadbasis("%d:%s" % (atomz, atomenv.basis), dtype=dtype, requires_grad=False) \
+        for atomz in atomenv.atomzs
+    ]
+    rgrid = atomenv.rgrid
 
     def evalgto(pos1, pos2, rgrid, name):
-        bases = loadbasis("1:%s" % basis, dtype=dtype, requires_grad=False)
-        atombasis1 = AtomCGTOBasis(atomz=1, bases=bases, pos=pos1)
-        atombasis2 = AtomCGTOBasis(atomz=1, bases=bases, pos=pos2)
-        env = LibcintWrapper([atombasis1, atombasis2], spherical=False)
+        atombasis1 = AtomCGTOBasis(atomz=atomenv.atomzs[0], bases=allbases[0], pos=pos1)
+        atombasis2 = AtomCGTOBasis(atomz=atomenv.atomzs[1], bases=allbases[1], pos=pos2)
+        env = LibcintWrapper([atombasis1, atombasis2], spherical=True)
         if name == "":
             return env.eval_gto(rgrid)
         elif name == "grad":
