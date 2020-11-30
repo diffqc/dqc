cdcba84458bbc510f2ca4049ad087af307c0d2e7
mfkasim1 <firman.kasim@gmail.com>
2020-11-30 16:30:33 +0000

    Add get_exc in Hamilton CGTO

--- dqc/hamilton/hamilton_cgto.py
+++ dqc/hamilton/hamilton_cgto.py
@@ -120,6 +120,11 @@ class HamiltonCGTO(BaseHamilton):
 
         return vxc_linop
 
+    def get_exc(self, xc: BaseXC, dm: torch.Tensor) -> torch.Tensor:
+        densinfo = self._dm2densinfo(dm, xc.family)  # value: (*BD, nr)
+        edens = xc.get_edensityxc(densinfo)  # (*BD, nr)
+        return torch.sum(self.grid.get_dvolume() * edens, dim=-1)
+
     def _dm2densinfo(self, dm: torch.Tensor, family: int) -> ValGrad:
         # dm: (*BD, nao, nao)
         # family: 1 for LDA, 2 for GGA, 3 for MGGA
