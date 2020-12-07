19fc848b2d93bb84f8f3cc698357a01d7847db72
mfkasim1 <firman.kasim@gmail.com>
2020-12-07 16:08:23 +0000

    Refactor the test_rks

--- dqc/test/test_rks.py
+++ dqc/test/test_rks.py
@@ -5,35 +5,38 @@ from dqc.system.mol import Mol
 
 dtype = torch.float64
 
+atomzs_poss = [
+    ([1, 1], [[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]]),
+    ([3, 3], [[-2.5, 0.0, 0.0], [2.5, 0.0, 0.0]]),
+    ([7, 7], [[-1.0, 0.0, 0.0], [1.0, 0.0, 0.0]]),
+    ([9, 9], [[-1.25, 0.0, 0.0], [1.25, 0.0, 0.0]]),
+    ([6, 8], [[-1.0, 0.0, 0.0], [1.0, 0.0, 0.0]]),
+]
+energies = [
+    -0.979143260,  # pyscf: -0.979143262
+    -14.3927863482007,  # pyscf: -14.3927863482007
+    -107.726124017789,  # pyscf: -107.726124017789
+    -197.005308558326,  # pyscf: -197.005308558326
+    -111.490687028797,  # pyscf: -111.490687028797
+]
+
 @pytest.mark.parametrize(
-    "xc,moldesc,energy_true",
-    [
-        ("lda,", "H -0.5 0 0; H 0.5 0 0", -0.979143260),  # pyscf: -0.979143262
-        ("lda,", "Li -2.5 0 0; Li 2.5 0 0", -14.3927863482007),  # pyscf: -14.3927863482007
-        ("lda,", "N -1 0 0; N 1 0 0", -107.726124017789),  # pyscf: -107.726124017789
-        ("lda,", "F -1.25 0 0; F 1.25 0 0", -197.005308558326),  # pyscf: -197.005308558326
-        ("lda,", "C -1 0 0; O 1 0 0", -111.490687028797),  # pyscf: -111.490687028797
-    ]
+    "xc,atomzs,poss,energy_true",
+    [("lda,", *atomz_pos, energy) for (atomz_pos, energy) in zip(atomzs_poss, energies)]
 )
-def test_rks_energy(xc, moldesc, energy_true):
-    mol = Mol(moldesc, basis="6-311++G**", dtype=dtype)
+def test_rks_energy(xc, atomzs, poss, energy_true):
+    mol = Mol((atomzs, poss), basis="6-311++G**", dtype=dtype)
     qc = RKS(mol, xc=xc).run()
     ene = qc.energy()
     assert torch.allclose(ene, ene * 0 + energy_true)
 
 @pytest.mark.parametrize(
     "xc,atomzs,poss",
-    [
-        ("lda,", [1, 1], [[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]]),
-        ("lda,", [3, 3], [[-2.5, 0.0, 0.0], [2.5, 0.0, 0.0]]),
-        ("lda,", [7, 7], [[-1.0, 0.0, 0.0], [1.0, 0.0, 0.0]]),
-        ("lda,", [9, 9], [[-1.25, 0.0, 0.0], [1.25, 0.0, 0.0]]),
-        ("lda,", [6, 8], [[-1.0, 0.0, 0.0], [1.0, 0.0, 0.0]]),
-    ]
+    [("lda,", *atomz_pos) for atomz_pos in atomzs_poss]
 )
 def test_rks_grad_pos(xc, atomzs, poss):
     def get_energy(poss_tensor):
-        mol = Mol((atomzs, poss_tensor), basis="3-21G", dtype=dtype)
+        mol = Mol((atomzs, poss_tensor), basis="3-21G", dtype=dtype, grid=3)
         qc = RKS(mol, xc=xc).run()
         return qc.energy()
     poss_tensor = torch.tensor(poss, dtype=dtype, requires_grad=True)
