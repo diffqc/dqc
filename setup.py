5d0a836a29cc1c8cf2d3935cb7ae33116b6373e4
mfkasim1 <firman.kasim@gmail.com>
2020-12-16 17:58:34 +0000

    Cleaning up the installation requirements

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-module_name = "ddft"
-ext_name = "%s.csrc" % module_name
-cpp_sources = [
-    "ddft/csrc/bind.cpp",
-]
-
-def get_pybind_include():
-    import pybind11
-    return pybind11.get_include()
-
-def get_torch_cpp_extension():
-    from torch.utils.cpp_extension import CppExtension
-    return CppExtension(
-        name=ext_name,
-        sources=cpp_sources,
-        include_dirs=[get_pybind_include()],
-        extra_compile_args=['-g'],
-    )
-
-def get_build_extension():
-    from torch.utils.cpp_extension import BuildExtension
-    return BuildExtension
-
 setup(
     name='ddft',
     version="0.1.0",
@@ -38,13 +15,9 @@ setup(
         "numpy>=1.8.2",
         "scipy>=0.15",
         "matplotlib>=1.5.3",
-        "pybind11>=2.5.0",
         "basis_set_exchange",
         # "pytorch>=1.8.0",
     ],
-    setup_requires=["pybind11>=2.5.0"],
-    ext_modules=[get_torch_cpp_extension()],
-    cmdclass={'build_ext': get_build_extension()},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
