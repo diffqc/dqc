a432553aaa4bb5a6a42d7929fcf8393657221f84
mfkasim1 <firman.kasim@gmail.com>
2020-12-29 00:24:12 +0000

    Test aodm2dens using pyscf as benchmark

--- dqc/test/test_hamilton.py
+++ dqc/test/test_hamilton.py
@@ -46,6 +46,7 @@ def test_cgto_ao2dm(system1):
 def test_cgto_dm2grid(system1):
     # test the function aodm2dens in hamilton cgto
 
+    torch.manual_seed(123)
     hamilton1 = system1.get_hamiltonian()
     nao = hamilton1.get_kinnucl().shape[-1]
     nb = 4
@@ -67,15 +68,37 @@ def test_cgto_dm2grid(system1):
     # 0th row: middle point between two atoms
     # 1st row: position of the first atom
     # 2nd row: position of the second atom
-    grids = torch.tensor(
-        [[0.0, 0.0, 0.0], [0.0, 0.0, 0.8], [0.0, 0.0, -0.8]],
-        dtype=dtype).unsqueeze(1)  # (3, 1, ndim)
+    grids = torch.tensor([
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.4],
+        [0.0, 0.0, 0.8],
+        [0.0, 0.0, -0.4],
+        [0.0, 0.0, -0.8]
+    ], dtype=dtype).unsqueeze(1)  # (ngrid, 1, ndim)
+    ngrid = grids.shape[0]
 
-    dens = hamilton1.aodm2dens(dm, grids)  # (3, nb)
-    assert list(dens.shape) == [3, nb]
-    assert torch.allclose(dens[..., 0], dens[..., 0] * 0)
-    assert torch.allclose(dens[..., 2], dens[..., 1] * 2)
-    assert torch.allclose(dens[..., 3], torch.tensor([0.8778, 2.1428, 2.1428], dtype=dtype))
+    # # pyscf code to obtain the actual results
+    # from pyscf import gto
+    # from pyscf.dft import numint
+    # mol = gto.M(atom="H 0 0 0.8; H 0 0 -0.8", basis="6-311++G**", unit="Bohr")
+    # ao = numint.eval_ao(mol, grids[:, 0, :].numpy())
+    # dens1 = numint.eval_rho(mol, ao, dm[1].numpy())
+    # dens2 = numint.eval_rho(mol, ao, dm[2].numpy())
+    # dens3 = numint.eval_rho(mol, ao, dm[3].numpy())
+    # print(dens1)
+    # print(dens2)
+    # print(dens3)
+    # raise RuntimeError
+
+    dens = hamilton1.aodm2dens(dm, grids)  # (ngrid, nb)
+    dens_true = torch.tensor([
+        [0.0, 0.0, 0.0, 0.0, 0.0],
+        [3.25841833, 4.37242451, 8.11849071, 4.0871612, 6.25558526],
+        [6.51683666, 8.74484901, 16.23698143, 8.1743224, 12.51117051],
+        [0.87780038, 1.05290457, 2.14278122, 1.05290457, 2.14278122],
+    ], dtype=dens.dtype).transpose(-2, -1)
+    assert list(dens.shape) == [ngrid, nb]
+    assert torch.allclose(dens, dens_true)
 
 def test_cgto_vext(system1):
     # test the external potential integration
