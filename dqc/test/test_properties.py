2008b7beb1ba69713cb34da573befcc0c039c81e
mfkasim1 <firman.kasim@gmail.com>
2021-04-19 11:51:59 +0100

    Test vibration

--- dqc/test/test_properties.py
+++ dqc/test/test_properties.py
@@ -27,8 +27,25 @@ def test_hess(h2o_qc):
     hess = hessian_pos(h2o_qc)
     assert torch.allclose(hess, hess.transpose(-2, -1).conj(), atol=2e-6)
 
-def atest_vibration(h2o_qc):
+def test_vibration(h2o_qc):
+    # test if the vibration of h2o is similar to what pyscf computes
+
     freq, normcoord = vibration(h2o_qc)
-    print(freq / 2.4188843265857e-17 / 2.99792458e8 / 1e2)
-    print(normcoord)
-    raise RuntimeError
+    freq_cm1 = freq / 2.4188843265857e-17 / 2.99792458e8 / 1e2
+
+    # pre-computed (the code to generate is below)
+    pyscf_freq_cm1 = torch.tensor([4074.51432922, 3915.25820884, 1501.856396], dtype=freq.dtype)
+
+    # # code to generate the frequencies above
+    # from pyscf import gto, dft
+    # from pyscf.prop.freq import rks
+    # mol = gto.M(atom='''
+    #             O 0 0 0.2217
+    #             H 0  1.4309 -0.8867
+    #             H 0 -1.4309 -0.8867''',
+    #             basis='321g', unit="Bohr")
+    # mf = dft.RKS(mol, xc="lda_x,lda_c_pw").run()
+    # w, modes = rks.Freq(mf).kernel()
+
+    # NOTE: rtol is a bit high, init?
+    assert torch.allclose(freq_cm1[:3], pyscf_freq_cm1, rtol=1e-2)
