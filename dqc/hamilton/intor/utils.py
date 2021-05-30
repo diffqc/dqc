cdf976db332061a3002773068ccbea3e79f97a23
mfkasim1 <firman.kasim@gmail.com>
2021-05-30 11:23:40 +0100

    Construct the path in the function

--- dqc/hamilton/intor/utils.py
+++ dqc/hamilton/intor/utils.py
@@ -16,16 +16,18 @@ NDIM = 3
 
 # libraries
 _ext = "dylib" if sys.platform == "darwin" else "so"
-_curpath = os.path.dirname(os.path.abspath(__file__))
-_libcint_path = os.path.join(_curpath, f"../../lib/deps/lib/libcint.{_ext}")
-_libcgto_path = os.path.join(_curpath, f"../../lib/libcgto.{_ext}")
-_libcpbc_path = os.path.join(_curpath, f"../../lib/libpbc.{_ext}")
-# _libcvhf_path = os.path.join(_curpath, "../../lib/libcvhf.{_ext}")
-_libcsymm_path = os.path.join(_curpath, f"../../lib/libsymm.{_ext}")
+_libcint_relpath = f"../../lib/deps/lib/libcint.{_ext}"
+_libcgto_relpath = f"../../lib/libcgto.{_ext}"
+_libcpbc_relpath = f"../../lib/libpbc.{_ext}"
+# _libcvhf_relpath = f"../../lib/libcvhf.{_ext}"
+_libcsymm_path = f"../../lib/libsymm.{_ext}"
 
 _libs: Dict[str, Any] = {}
 
-def _library_loader(name: str, path: str) -> Callable:
+def _library_loader(name: str, relpath: str) -> Callable:
+    curpath = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.abspath(os.path.join(curpath, relpath))
+
     # load the library and cache the handler
     def fcn():
         if name not in _libs:
@@ -39,11 +41,11 @@ def _library_loader(name: str, path: str) -> Callable:
         return _libs[name]
     return fcn
 
-CINT = _library_loader("cint", _libcint_path)
-CGTO = _library_loader("cgto", _libcgto_path)
-CPBC = _library_loader("cpbc", _libcpbc_path)
-# CVHF = _library_loader("CVHF", _libcvhf_path)
-CSYMM = _library_loader("symm", _libcsymm_path)
+CINT = _library_loader("cint", _libcint_relpath)
+CGTO = _library_loader("cgto", _libcgto_relpath)
+CPBC = _library_loader("cpbc", _libcpbc_relpath)
+# CVHF = _library_loader("CVHF", _libcvhf_relpath)
+CSYMM = _library_loader("symm", _libcsymm_relpath)
 
 c_null_ptr = ctypes.POINTER(ctypes.c_void_p)
 
