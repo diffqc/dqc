b2872a9e35c8a8e7c6aacc3bffa0c48d899e4d0e
mfkasim1 <firman.kasim@gmail.com>
2021-05-31 17:20:08 +0100

    Get long descriptor from README

--- setup.py
+++ setup.py
@@ -5,10 +5,26 @@ import shutil
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
+# module's descriptor
 module_name = "dqc"
 ext_name = "dqc.lib.pyscflibs"
+github_url = "https://github.com/diffqc/dqc/tree/master/"
+raw_github_url = "https://raw.githubusercontent.com/diffqc/dqc/master/"
+
 file_dir = os.path.dirname(os.path.realpath(__file__))
 absdir = lambda p: os.path.join(file_dir, p)
+
+# get the long description from README
+# open readme and convert all relative path to absolute path
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
+link_pattern = re.compile(r"\(([\w\-/]+)\)")
+img_pattern  = re.compile(r"\(([\w\-/\.]+)\)")
+link_repl = r"(%s\1)" % github_url
+img_repl  = r"(%s\1)" % raw_github_url
+long_desc = re.sub(link_pattern, link_repl, long_desc)
+long_desc = re.sub(img_pattern, img_repl, long_desc)
 
 ############### versioning ###############
 verfile = os.path.abspath(os.path.join(module_name, "_version.py"))
@@ -76,6 +92,8 @@ setup(
     version=vers,
     description='Differentiable Quantum Chemistry',
     url='https://github.com/diffqc/dqc/',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     author='mfkasim1',
     author_email='firman.kasim@gmail.com',
     license='Apache License 2.0',
